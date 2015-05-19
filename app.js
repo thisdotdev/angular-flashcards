ad04c51a180d3f1d5be76a258dc5b084fc28438b
Mel Quark <mel.quark@gmail.com>
2015-05-19 15:51:17 -0700

    cycle through cards. front and back separately

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ flashApp.controller("CardController", function ($scope) {
       back: "this is the back"
     }
   ];
-  $scope.current = 0;
-  $scope.nextCard = function(){
-    $scope.current = ($scope.current+1) % ($scope.cards.length);
+
+  $scope.current = 0; // card to display
+  $scope.front = true;
+
+  $scope.next = function(){
+    var move = +(!$scope.front); // change cards if on back
+    var numCards = $scope.cards.length;
+
+    $scope.current = ($scope.current + move) % numCards;
+    $scope.front = !$scope.front;
   };
+
 });
