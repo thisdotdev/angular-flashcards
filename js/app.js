78e02dfd25d65b5aae23984923e329449349cfa3
Mel Quark <mel.quark@gmail.com>
2015-06-11 11:20:32 -0700

    add routing

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-var flashApp = angular.module("flash",
-  ["flash.deck", "flash.study", "flash.cards"]);
+var flashApp = angular.module("flash", [
+  "ngRoute", 
+  "flash.deck", 
+  "flash.study", 
+  "flash.cards"
+]).config(["$routeProvider", function($routeProvider){
+  $routeProvider
+    .when("/study", {
+      templateUrl: "partials/study.html",
+      controller: "StudyController"
+    })
+    .when("/add", {
+      templateUrl: "partials/cards.html",
+      controller: "CardsController"
+    });
+}]);
